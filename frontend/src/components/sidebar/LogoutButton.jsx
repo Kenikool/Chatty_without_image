c00412b3eba07002e70090745c846aca06894ab7
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-05-07 20:02:21 +0100

    sidebar_setup

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,17 +1,28 @@
-import { BiLogOut } from "react-icons/bi";
+import React from "react";
+
+const LogoutButton = () => {
+  return <div>LogoutButton</div>;
+};
+
+export default LogoutButton;
+
+/* import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-	const { loading, logout } = useLogout();
+  const { loading, logout } = useLogout();
 
-	return (
-		<div className='mt-auto'>
-			{!loading ? (
-				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
-			) : (
-				<span className='loading loading-spinner'></span>
-			)}
-		</div>
-	);
+  return (
+    <div className="mt-auto">
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
+    </div>
+  );
 };
-export default LogoutButton;
+export default LogoutButton; */
